09e8876f70396be4881ba1f5382d2b696d98b9b9
David Santamaría Rogado <howl.nsp@gmail.com>
2026-04-30 21:49:08 +0200

    js/main.js: move imagenes araay into main function
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,42 +1,3 @@
-const imagenes = [
-  {
-    src: 'assets/images/viajes/viajes-1.jpg',
-    pais: 'Cuba',
-    descripcion: 'En Varadero encontrarás las mejores playas cristalinas.',
-    tags: ['playa', 'tropical', 'costa', 'sol', 'calor'],
-  }, {
-    src: 'assets/images/viajes/viajes-2.jpg',
-    pais: 'Maldivas',
-    descripcion: 'Disfruta de los mejores atolones del mundo.',
-    tags: ['playa', 'tropical', 'costa', 'atolón', 'sol', 'calor'],
-  }, {
-    src: 'assets/images/viajes/viajes-3.jpg',
-    pais: 'Múltiples destinos',
-    descripcion: 'Si no te decides que país quieres visitar hay disponibles viajes por varios de ellos a la vez.',
-    tags: ['letreros'],
-  }, {
-    src: 'assets/images/viajes/viajes-4.jpg',
-    pais: 'España',
-    descripcion: 'Sevilla tiene un color especial.',
-    tags: ['ciudad', 'sol', 'calor'],
-  }, {
-    src: 'assets/images/viajes/viajes-5.jpg',
-    pais: 'España',
-    descripcion: 'La misma plaza desde otro lado, o quizás es Naboo.',
-    tags: ['ciudad', 'sol', 'calor'],
-  }, {
-    src: 'assets/images/viajes/viajes-6.jpg',
-    pais: 'España',
-    descripcion: 'Paseo del Arañón.',
-    tags: ['costa', 'frio'],
-  }, {
-    src: 'assets/images/viajes/viajes-7.jpg',
-    pais: 'España',
-    descripcion: 'Castillo de la Yedra.',
-    tags: ['histórico', 'sol', 'calor'],
-  },
-];
-
 /**
  * Genera un array con los tags únicos presentes en una serie de elementos con tags.
  * @param {[]} elementos - Array de elementos de los que leer sus tags.
@@ -73,6 +34,44 @@ const generarBotonesDesdeTags = (tags) => {
  * Función de ejecución principal.
  */
 (() => {
+  const imagenes = [
+    {
+      src: 'assets/images/viajes/viajes-1.jpg',
+      pais: 'Cuba',
+      descripcion: 'En Varadero encontrarás las mejores playas cristalinas.',
+      tags: ['playa', 'tropical', 'costa', 'sol', 'calor'],
+    }, {
+      src: 'assets/images/viajes/viajes-2.jpg',
+      pais: 'Maldivas',
+      descripcion: 'Disfruta de los mejores atolones del mundo.',
+      tags: ['playa', 'tropical', 'costa', 'atolón', 'sol', 'calor'],
+    }, {
+      src: 'assets/images/viajes/viajes-3.jpg',
+      pais: 'Múltiples destinos',
+      descripcion: 'Si no te decides que país quieres visitar hay disponibles viajes por varios de ellos a la vez.',
+      tags: ['letreros'],
+    }, {
+      src: 'assets/images/viajes/viajes-4.jpg',
+      pais: 'España',
+      descripcion: 'Sevilla tiene un color especial.',
+      tags: ['ciudad', 'sol', 'calor'],
+    }, {
+      src: 'assets/images/viajes/viajes-5.jpg',
+      pais: 'España',
+      descripcion: 'La misma plaza desde otro lado, o quizás es Naboo.',
+      tags: ['ciudad', 'sol', 'calor'],
+    }, {
+      src: 'assets/images/viajes/viajes-6.jpg',
+      pais: 'España',
+      descripcion: 'Paseo del Arañón.',
+      tags: ['costa', 'frio'],
+    }, {
+      src: 'assets/images/viajes/viajes-7.jpg',
+      pais: 'España',
+      descripcion: 'Castillo de la Yedra.',
+      tags: ['histórico', 'sol', 'calor'],
+    },
+  ];
   const botonesFiltrado = document.querySelector('#botonesFiltrado');
 
   document.addEventListener('click', ev => {
