655a7e89490e9a9168581aa1f76134b5552f1997
David Santamaría Rogado <howl.nsp@gmail.com>
2026-04-30 18:41:21 +0200

    main.js: add imagenes array
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const imagenes = [
+  {
+    src: 'assets/images/viajes/viajes-1.jpg',
+    pais: 'Cuba',
+    descripcion: 'En Varadero encontrarás las mejores playas cristalinas.',
+    tags: ['playa', 'tropical', 'costa', 'sol', 'calor'],
+  }, {
+    src: 'assets/images/viajes/viajes-2.jpg',
+    pais: 'Maldivas',
+    descripcion: 'Disfruta de los mejores atolones del mundo.',
+    tags: ['playa', 'tropical', 'costa', 'atolón', 'sol', 'calor'],
+  }, {
+    src: 'assets/images/viajes/viajes-3.jpg',
+    pais: 'Múltiples destinos',
+    descripcion: 'Si no te decides que país quieres visitar hay disponibles viajes por varios de ellos a la vez.',
+    tags: ['letreros'],
+  }, {
+    src: 'assets/images/viajes/viajes-4.jpg',
+    pais: 'España',
+    descripcion: 'Sevilla tiene un color especial.',
+    tags: ['ciudad', 'sol', 'calor'],
+  }, {
+    src: 'assets/images/viajes/viajes-5.jpg',
+    pais: 'España',
+    descripcion: 'La misma plaza desde otro lado, o quizás es Naboo.',
+    tags: ['ciudad', 'sol', 'calor'],
+  }, {
+    src: 'assets/images/viajes/viajes-6.jpg',
+    pais: 'España',
+    descripcion: 'Paseo del Arañón.',
+    tags: ['costa', 'frio'],
+  }, {
+    src: 'assets/images/viajes/viajes-7.jpg',
+    pais: 'España',
+    descripcion: 'Castillo de la Yedra.',
+    tags: ['histórico', 'sol', 'calor'],
+  },
+];
