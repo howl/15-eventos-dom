ec07ae619d80321dfa961c8871660efde7900781
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-01 18:57:46 +0200

    js/main.js: add 'todas' tag
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -85,37 +85,37 @@ const mostrarImagenesConTag = (imagenes, tag, elementoPadre) => {
       src: 'assets/images/viajes/viajes-1.jpg',
       pais: 'Cuba',
       descripcion: 'En Varadero encontrarás las mejores playas cristalinas.',
-      tags: ['playa', 'tropical', 'costa', 'sol', 'calor'],
+      tags: ['todas', 'playa', 'tropical', 'costa', 'sol', 'calor'],
     }, {
       src: 'assets/images/viajes/viajes-2.jpg',
       pais: 'Maldivas',
       descripcion: 'Disfruta de los mejores atolones del mundo.',
-      tags: ['playa', 'tropical', 'costa', 'atolón', 'sol', 'calor'],
+      tags: ['todas', 'playa', 'tropical', 'costa', 'atolón', 'sol', 'calor'],
     }, {
       src: 'assets/images/viajes/viajes-3.jpg',
       pais: 'Múltiples destinos',
       descripcion: 'Si no te decides que país quieres visitar hay disponibles viajes por varios de ellos a la vez.',
-      tags: ['letreros'],
+      tags: ['todas', 'letreros'],
     }, {
       src: 'assets/images/viajes/viajes-4.jpg',
       pais: 'España',
       descripcion: 'Sevilla tiene un color especial.',
-      tags: ['ciudad', 'sol', 'calor', 'histórico'],
+      tags: ['todas', 'ciudad', 'sol', 'calor', 'histórico'],
     }, {
       src: 'assets/images/viajes/viajes-5.jpg',
       pais: 'España',
       descripcion: 'La misma plaza desde otro lado, o quizás es Naboo.',
-      tags: ['ciudad', 'sol', 'calor', 'histórico'],
+      tags: ['todas', 'ciudad', 'sol', 'calor', 'histórico'],
     }, {
       src: 'assets/images/viajes/viajes-6.jpg',
       pais: 'España',
       descripcion: 'Paseo del Arañón.',
-      tags: ['costa', 'frio'],
+      tags: ['todas', 'costa', 'frio'],
     }, {
       src: 'assets/images/viajes/viajes-7.jpg',
       pais: 'España',
       descripcion: 'Castillo de la Yedra.',
-      tags: ['histórico', 'sol', 'calor'],
+      tags: ['todas', 'histórico', 'sol', 'calor'],
     },
   ];
   const botonesFiltrado = document.querySelector('#botonesFiltrado');
