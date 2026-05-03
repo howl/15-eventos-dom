80b49ff7b0d5c68b945dd0f4b5659d0f0af2ff16
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-03 22:14:54 +0200

    refer to elements as viajes instead of imagenes
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 /**
- * Genera un array con los tags únicos presentes en una serie de elementos con tags.
- * @param {[]} elementos - Array de elementos de los que leer sus tags.
+ * Genera un array con los tags únicos presentes en una serie de viajes con
+ * tags.
+ * @param {[]} viajes - Array de viajes de los que leer sus tags.
  * @returns {[]} - El array con los tags únicos.
  */
-const generarTagsDesdeElementos = (elementos, primerTag) => {
+const generarTagsDesdeViajes = (viajes, primerTag) => {
   /*
-    De las múltiples formas que se pueden hacer para obtener los elementos únicos,
-    Ej: iterar cada array comprobando con includes en el array de destino si ya existe,
-    elijo usar el objeto Set propio de javascript. Es mucho más rapido haciendo lo suyo.
+    De las múltiples formas que se pueden hacer para obtener los tags únicos,
+    Ej: iterar cada array comprobando con includes en el array de destino si ya
+    existe, elijo usar el objeto Set propio de javascript. Es mucho más rapido
+    haciendo lo suyo.
   */
   const tags = new Set();
 
-  elementos.forEach((elemento) => elemento.tags.forEach((tag) => tags.add(tag)));
+  viajes.forEach((viaje) => viaje.tags.forEach((tag) => tags.add(tag)));
 
-  /* Colo el tag especificado en primerTag el primero y los demás de forma normal */
+  /* Colo el tag en primerTag el primero y el resto ordenados de forma normal */
   return Array.from(tags).sort((a, b) => a === primerTag ? false : b === primerTag ? true : a > b);
 };
 
 /**
- * Genera los botones de filtrado en base a las etiquetas que se le pasan.
+ * Genera los botones de filtrado en base a los tags que se le pasan.
  * @param {[]} tags - Un array con los tags para los que hacer botones.
  * @param {Element} elementoPadre - Elemento al que colocar las botones.
  */
@@ -35,107 +37,108 @@ const generarBotonesDesdeTags = (tags, elementoPadre) => {
 };
 
 /**
- * Filtra las imagenes por el tag añdiéndolas al elementoPadre devolviendo la
- * cantidad de imágenes filtradas.
- * @param {[]} imagenes - Array con las imagenes a filtrar.
- * @param {string} tag - Tag por el que filtrar las imagenes.
- * @param {Element} elementoPadre - Elemento al que colocar las imágenes.
- * @returns {number} - Cantidad de imágenes filtradas.
+ * Filtra los viajes por el tag añdiéndolos al elementoPadre devolviendo la
+ * cantidad de viajes filtrados.
+ * @param {[]} viajes - Array con los viajes a filtrar.
+ * @param {string} tag - Tag por el que filtrar los viajes.
+ * @param {Element} elementoPadre - Elemento al que colocar los viajes.
+ * @returns {number} - Cantidad de viajes filtrados.
  */
-const mostrarImagenesConTag = (imagenes, tag, elementoPadre) => {
+const mostrarViajesConTag = (viajes, tag, elementoPadre) => {
   const fragmento = document.createDocumentFragment();
-  const imagenesFiltradas = imagenes.filter(imagen => imagen.tags.includes(tag));
+  const viajesFiltrados = viajes.filter(viaje => viaje.tags.includes(tag));
 
   elementoPadre.textContent = '';
   /*
     Es importante colocar el primero en la estructura HTML el título de la
-    sección secundaria, ya que sino el efecto de scroll suave para la imagen que
-    es principal por defecto al pasarla    a secundaria y retornarla a principal
-    hace un desplazamiento brusco a la parte superior de la página y luego baja suave.
+    sección secundaria, ya que sino el efecto de scroll suave para el viaje que
+    es principal por defecto al pasarlo a secundaria y retornarlo a principal
+    hace un desplazamiento brusco a la parte superior de la página y luego baja
+    suave.
     Así lo hace desde abajo sin movimiento brusco inicial como las demás.
   */
-  if (imagenesFiltradas.length > 1) {
-    const imagenesRelacionadasTitulo = document.createElement('h3');
+  if (viajesFiltrados.length > 1) {
+    const viajesRelacionadosTitulo = document.createElement('h3');
 
-    imagenesRelacionadasTitulo.classList.add('flexItem');
-    imagenesRelacionadasTitulo.textContent = 'Viajes relacionados';
-    fragmento.appendChild(imagenesRelacionadasTitulo);
+    viajesRelacionadosTitulo.classList.add('flexItem');
+    viajesRelacionadosTitulo.textContent = 'Viajes relacionados';
+    fragmento.appendChild(viajesRelacionadosTitulo);
   }
-  imagenesFiltradas.forEach((imagen, index) => {
-    const elemento = document.createElement('div');
+  viajesFiltrados.forEach((viaje, index) => {
+    const viajeElemento = document.createElement('div');
     const contenedorInterior = document.createElement('div');
-    const imagenFigure = document.createElement('figure');
-    const imagenImagen = document.createElement('img');
-    const imagenCaption = document.createElement('figcaption');
-    const imagenCaptionLabel = document.createElement('span');
-    const imagenDescripcionExtendida = document.createElement('p');
-    const imagenDescripcionExtendidaLabel = document.createElement('span');
+    const viajeFigure = document.createElement('figure');
+    const viajeImagen = document.createElement('img');
+    const viajeCaption = document.createElement('figcaption');
+    const viajeCaptionLabel = document.createElement('span');
+    const viajeDescripcionExtendida = document.createElement('p');
+    const viajeDescripcionExtendidaLabel = document.createElement('span');
 
-    elemento.classList.add('cajaTransparente');
-    elemento.classList.add('flexItem', !index ? 'flexItemPrincipal' : 'flexItemSecundario');
-    elemento.appendChild(contenedorInterior);
-    contenedorInterior.appendChild(imagenFigure);
-    imagenFigure.appendChild(imagenImagen);
-    imagenImagen.src = imagen.src;
-    imagenImagen.alt = imagen.pais;
-    imagenFigure.appendChild(imagenCaption);
-    imagenCaption.appendChild(imagenCaptionLabel);
-    imagenCaptionLabel.classList.add('negrita');
-    imagenCaptionLabel.appendChild(document.createTextNode('Título'));
-    imagenCaption.appendChild(document.createTextNode(`: ${imagen.pais}`));
-    contenedorInterior.appendChild(imagenDescripcionExtendida);
-    imagenDescripcionExtendida.classList.add('cursiva');
-    imagenDescripcionExtendida.appendChild(imagenDescripcionExtendidaLabel);
-    imagenDescripcionExtendidaLabel.classList.add('cursiva');
-    imagenDescripcionExtendidaLabel.classList.add('negrita');
-    imagenDescripcionExtendidaLabel.appendChild(document.createTextNode('Descripción'));
-    imagenDescripcionExtendida.appendChild(document.createTextNode(`: ${imagen.descripcion}`));
+    viajeElemento.classList.add('cajaTransparente');
+    viajeElemento.classList.add('flexItem', !index ? 'flexItemPrincipal' : 'flexItemSecundario');
+    viajeElemento.appendChild(contenedorInterior);
+    contenedorInterior.appendChild(viajeFigure);
+    viajeFigure.appendChild(viajeImagen);
+    viajeImagen.src = viaje.imgSrc;
+    viajeImagen.alt = viaje.pais;
+    viajeFigure.appendChild(viajeCaption);
+    viajeCaption.appendChild(viajeCaptionLabel);
+    viajeCaptionLabel.classList.add('negrita');
+    viajeCaptionLabel.appendChild(document.createTextNode('Título'));
+    viajeCaption.appendChild(document.createTextNode(`: ${viaje.pais}`));
+    contenedorInterior.appendChild(viajeDescripcionExtendida);
+    viajeDescripcionExtendida.classList.add('cursiva');
+    viajeDescripcionExtendida.appendChild(viajeDescripcionExtendidaLabel);
+    viajeDescripcionExtendidaLabel.classList.add('cursiva');
+    viajeDescripcionExtendidaLabel.classList.add('negrita');
+    viajeDescripcionExtendidaLabel.appendChild(document.createTextNode('Descripción'));
+    viajeDescripcionExtendida.appendChild(document.createTextNode(`: ${viaje.descripcion}`));
 
-    fragmento.appendChild(elemento);
+    fragmento.appendChild(viajeElemento);
   });
 
   elementoPadre.appendChild(fragmento);
 
-  return imagenesFiltradas.length;
+  return viajesFiltrados.length;
 };
 
 /**
  * Función de ejecución principal.
  */
 (() => {
-  const imagenes = [
+  const viajes = [
     {
-      src: 'assets/images/viajes/viajes-1.jpg',
+      imgSrc: 'assets/images/viajes/viajes-1.jpg',
       pais: 'Cuba',
       descripcion: 'En Varadero encontrarás las mejores playas cristalinas.',
       tags: ['todas', 'playa', 'tropical', 'costa', 'sol', 'calor'],
     }, {
-      src: 'assets/images/viajes/viajes-2.jpg',
+      imgSrc: 'assets/images/viajes/viajes-2.jpg',
       pais: 'Maldivas',
       descripcion: 'Disfruta de los mejores atolones del mundo.',
       tags: ['todas', 'playa', 'tropical', 'costa', 'atolón', 'sol', 'calor'],
     }, {
-      src: 'assets/images/viajes/viajes-3.jpg',
+      imgSrc: 'assets/images/viajes/viajes-3.jpg',
       pais: 'Múltiples destinos',
       descripcion: 'Si no te decides que país quieres visitar hay disponibles viajes por varios de ellos a la vez.',
       tags: ['todas', 'letreros'],
     }, {
-      src: 'assets/images/viajes/viajes-4.jpg',
+      imgSrc: 'assets/images/viajes/viajes-4.jpg',
       pais: 'España',
       descripcion: 'Sevilla tiene un color especial.',
       tags: ['todas', 'ciudad', 'sol', 'calor', 'histórico'],
     }, {
-      src: 'assets/images/viajes/viajes-5.jpg',
+      imgSrc: 'assets/images/viajes/viajes-5.jpg',
       pais: 'España',
       descripcion: 'La misma plaza desde otro lado, o quizás es Naboo.',
       tags: ['todas', 'ciudad', 'sol', 'calor', 'histórico'],
     }, {
-      src: 'assets/images/viajes/viajes-6.jpg',
+      imgSrc: 'assets/images/viajes/viajes-6.jpg',
       pais: 'España',
       descripcion: 'Paseo del Arañón.',
       tags: ['todas', 'costa', 'frio'],
     }, {
-      src: 'assets/images/viajes/viajes-7.jpg',
+      imgSrc: 'assets/images/viajes/viajes-7.jpg',
       pais: 'España',
       descripcion: 'Castillo de la Yedra.',
       tags: ['todas', 'histórico', 'sol', 'calor'],
@@ -144,7 +147,10 @@ const mostrarImagenesConTag = (imagenes, tag, elementoPadre) => {
   const botonesFiltrado = document.querySelector('#botonesFiltrado');
 
   document.addEventListener('click', ev => {
-    /* Como uso un único listener general, no secciono cada parte en funciones distintas ya que no va a existir reuso de código */
+    /*
+      Como uso un único listener general, no secciono cada parte en funciones
+      distintas ya que no va a existir reuso de código
+    */
 
     /* Click sobre cualquier tag no pulsado actualmente */
     {
@@ -152,7 +158,7 @@ const mostrarImagenesConTag = (imagenes, tag, elementoPadre) => {
       if (botonPulsado) {
         const botonPulsadoAnterior = document.querySelector('#botonesFiltrado>button.pulsado');
         const pluralSingular = document.querySelectorAll('#infoDeFiltrado>.pluralSingular');
-        const numImagesFiltradasNumber = mostrarImagenesConTag(imagenes, botonPulsado.textContent, document.querySelector('#galeriaFotos'))
+        const numViajesFiltradosNumber = mostrarViajesConTag(viajes, botonPulsado.textContent, document.querySelector('#galeriaViajes'))
 
         if (!botonPulsadoAnterior) {
           const infoDeUso = document.querySelector('#infoDeUso');
@@ -164,8 +170,8 @@ const mostrarImagenesConTag = (imagenes, tag, elementoPadre) => {
         else
           botonPulsadoAnterior.classList.remove('pulsado');
         botonPulsado.classList.add('pulsado');
-        document.querySelector('#numImagesFiltradas').textContent = numImagesFiltradasNumber;
-        if (numImagesFiltradasNumber === 1) {
+        document.querySelector('#numViajesFiltrados').textContent = numViajesFiltradosNumber;
+        if (numViajesFiltradosNumber === 1) {
           pluralSingular[0].textContent = 'ha';
           pluralSingular[1].textContent = 'viaje';
         } else {
@@ -179,12 +185,12 @@ const mostrarImagenesConTag = (imagenes, tag, elementoPadre) => {
 
     /*
       Click sobre cualquier elemento que tengo algún antecesor siendo un div con
-      clase flexItem pero no flexItemPrincipal e hijo directo de galeriaFotos
+      clase flexItem pero no flexItemPrincipal e hijo directo de galeriaViajes
     */
     {
-      const flexItemPulsado = ev.target.closest('#galeriaFotos>div.flexItem:not(.flexItemPrincipal)');
+      const flexItemPulsado = ev.target.closest('#galeriaViajes>div.flexItem:not(.flexItemPrincipal)');
       if (flexItemPulsado) {
-        const flexItemPrincipal = document.querySelector('#galeriaFotos>.flexItemPrincipal');
+        const flexItemPrincipal = document.querySelector('#galeriaViajes>.flexItemPrincipal');
 
         flexItemPrincipal.classList.remove('flexItemPrincipal');
         flexItemPrincipal.classList.add('flexItemSecundario');
@@ -195,5 +201,5 @@ const mostrarImagenesConTag = (imagenes, tag, elementoPadre) => {
     }
   });
 
-  generarBotonesDesdeTags(generarTagsDesdeElementos(imagenes, 'todas'), botonesFiltrado);
+  generarBotonesDesdeTags(generarTagsDesdeViajes(viajes, 'todas'), botonesFiltrado);
 })();
